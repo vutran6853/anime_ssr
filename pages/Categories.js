d925000ac57cc366697cc265ffeab16b996e67ad
your <vutran6853@gmail.com>
2019-10-26 00:48:29 -0500

    add categories page and component for categories

--- pages/Categories.js
+++ pages/Categories.js
@@ -1,8 +1,17 @@
 import Layout from './Layout'
+import './categories.scss'
+import Link from 'next/link'
 
 class Categories extends React.Component {
-  state = {
-    categoriesList: null
+  constructor(props) {
+    super(props)
+    this.state = {
+      categoriesList: [],
+      animeInfo: [],
+      categoriesID: 0,
+      animeInfoBool: false
+    }
+    this.handleSelectCat = this.handleSelectCat.bind(this)
   }
 
   componentDidMount() {
@@ -10,9 +19,8 @@ class Categories extends React.Component {
     fetch('https://kitsu.io/api/edge/categories')
       .then((response) => response.json())
       .then((response) => {
-        console.log('response=', response.data)
         let result = []
-        response.data.map((value, index) => {
+        response.data.map((value) => {
           // console.log(`value[${index}] =`, value.attributes);
           result.push(value.attributes)
         })
@@ -25,17 +33,44 @@ class Categories extends React.Component {
   }
 
   handleSelectCat(e) {
-    console.log('payload =', e.target.value)
+    this.setState({ categoriesID: parseInt(e.target.value) }, function anonymous() {
+      this.handleFetchAnimeInfo()
+    })
+  }
+
+  handleFetchAnimeInfo() {
+    console.log('handleFetchAnimeInfo', this.state.categoriesID)
+
+    fetch(`https://kitsu.io/api/edge/categories/${this.state.categoriesID}/anime`)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log('response=', response.data)
+        if (response.errors) {
+          this.setState({ animeInfoBool: false })
+          throw new Error()
+        }
+
+        this.setState({
+          animeInfoBool: true,
+          animeInfo: response.data
+          // animeInfo: response.data.map((value) => value.attributes)
+        })
+      })
+      .catch((err) => {
+        console.log('Unable fetch handleFetchAnimeInfo()', err)
+      })
   }
 
   render() {
-    console.log(this.state)
+    // console.log(this.state)
 
-    const renderCategoriesOption = this.state.categoriesList
-      ? this.state.categoriesList.map((value, index) => {
-          return <option value={value.title}>{value.title}</option>
-        })
-      : ''
+    const renderCategoriesOption = this.state.categoriesList.map((value, index) => {
+      return (
+        <option value={index} key={value.title}>
+          {value.title}
+        </option>
+      )
+    })
 
     const renderCategoriesSelect = this.state.categoriesList ? (
       <select onChange={this.handleSelectCat}>{renderCategoriesOption}</select>
@@ -43,10 +78,43 @@ class Categories extends React.Component {
       <div>false</div>
     )
 
+    const isAnimeList = this.state.animeInfoBool ? (
+      this.state.animeInfo.map(function anonymous(value, index) {
+        console.log(`value[${index}] = `, value)
+        return (
+          <Link href={`/anime/AnimeInfo?id=${value.id}`} as={`/anime?id=${value.id}`} key={value.canonicalTitle}>
+            
+            <div className="anime_card_item">
+              <h3>{value.attributes.canonicalTitle}</h3>
+              <img src={value.attributes.posterImage.small} alt={value.attributes.posterImage.small}></img>
+              <div className="anime_card_rate">
+                <p>Rate: {value.attributes.averageRating}</p>
+                <p>Tag: {value.attributes.ageRatingGuide}</p>
+                <p>Age: {value.attributes.ageRating}</p>
+              </div>
+            </div>
+          </Link>
+        )
+      })
+    ) : (
+      <div>false</div>
+    )
+
+    // const renderAnimeList = this.state.animeInfo.map(function anonymous(value, index) {
+    //   console.log(`value[${index}] = `, value);
+    //   return (
+    //     <div>{value.id}</div>
+    //   )
+    // })
+
+    // const isAnimeList = this.state.categoriesID
+
     return (
       <Layout>
         <div>Categories Components</div>
         {renderCategoriesSelect}
+        <div className="anime_card_container">{isAnimeList}</div>
+        {/* {renderAnimeList} */}
       </Layout>
     )
   }
